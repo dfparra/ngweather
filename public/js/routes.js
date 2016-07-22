c9c7117e90636f42ec61ca00bf14996e483c4634
Daniel <danielparra93@gmail.com>
2016-07-22 19:30:07 -0400

    setup ngrouter

---
+++ public/js/routes.js
@@ -0,0 +1,30 @@
+
+
+(function(){
+  angular.module('ngweather')
+          .config(RouteConfig);
+
+  RouteConfig.$inject = ['$routeProvider','$locationProvider'];
+
+  function RouteConfig($routeProvider, $locationProvider){
+    $routeProvider
+    .when('/', {
+      template: 'home'
+    })
+    .when('/hourly', {
+      template: 'hourly'
+    })
+    .when('/minutely', {
+      template: 'minutely'
+    })
+    .when('/daily', {
+      template: 'daily'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+
+
+    // $locationProvider.html5Mode(true);
+  }
+})();
