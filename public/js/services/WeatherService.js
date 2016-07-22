77e2c42bdece16c3a8d775229c44a6974d0c2997
Daniel <danielparra93@gmail.com>
2016-07-21 20:53:11 -0400

    DRY principle on some code

--- public/js/services/WeatherService.js
+++ public/js/services/WeatherService.js
@@ -7,6 +7,11 @@
   function WeatherService($http){
     var passphrase = 'i like cheese and bacon and some other things too';
     var baseUrl = 'https://quiet-bayou-88937.herokuapp.com/';
+    var config = {
+      headers: {
+        'passphrase': passphrase
+      }
+    };
     var service = {
       getHourlyData: getHourlyData,
       getMinutelyData: getMinutelyData,
@@ -17,44 +22,32 @@
 
     function getHourlyData(lat, lon){
       var url = baseUrl + 'forecast/hourly/'+ lat + ',' + lon;
-      console.log(service.weather);
-      var config = {
-        headers: {
-          'passphrase': passphrase
-        }
-      };
+      // console.log(service.weather);
+
       return $http.get(url, config)
                     .then(function(response){
                       service.weather = response.data;
-                      console.log(service.weather);
+                      // console.log(service.weather);
                     });
     }
     function getMinutelyData(lat, lon){
       var url = baseUrl + 'forecast/minutely/' + lat + ',' + lon;
-      console.log(service.weather);
-      var config = {
-        headers: {
-          'passphrase': passphrase
-        }
-      };
+      // console.log(service.weather);
+
       return $http.get(url, config)
                   .then(function(response){
                     service.weather = response.data;
-                    console.log(service.weather);
+                    // console.log(service.weather);
                   });
     }
     function getDailyData(lat, lon){
       var url = baseUrl + 'forecast/daily/' + lat + ',' + lon;
-      console.log(service.weather);
-      var config = {
-        headers: {
-          'passphrase': passphrase
-        }
-      };
+      // console.log(service.weather);
+
       return $http.get(url, config)
                   .then(function(response){
                     service.weather = response.data;
-                    console.log(service.weather);
+                    // console.log(service.weather);
                   });
     }
   }
