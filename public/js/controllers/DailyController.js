3d19cfd603d37025d0c8545f6e312b829e37f4c8
Daniel <danielparra93@gmail.com>
2016-07-27 13:09:29 -0400

    Daily branch (no minutely) complete

--- public/js/controllers/DailyController.js
+++ public/js/controllers/DailyController.js
@@ -2,10 +2,21 @@
   angular.module('ngweather')
           .controller('DailyController', DailyController);
 
-  DailyController.$inject = [];
+  DailyController.$inject = ['$scope', 'WeatherService'];
 
-  function DailyController(){
-    
+  function DailyController($scope, WeatherService){
+    $scope.dailyData = WeatherService.weather;
+    // $scope.summaryLookup = {
+    //   'Drizzle': 'There will be some drizzle',
+    //   'Partly Cloudy': 'The skies will be partly cloudy',
+    // };
+
+    $scope.$watch(function(){
+      return WeatherService.weather;
+    }, function(value){
+      // console.log(value);
+      $scope.dailyData = value;
+    });
+
   }
-
 })();
